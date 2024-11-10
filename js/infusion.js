f677e78b8366fcda9d57f271533e8d1ffc9c2356
NoRocketScience <norocketscience@duck.com>
2024-11-10 13:35:22 +0100

    fixed infusion checkboxes not being checked

--- js/infusion.js
+++ js/infusion.js
@@ -9,47 +9,51 @@ class Infusion {
         this.isCustom = isCustom;
 
         Infusion.updateInfusedItems = function () {
-            Storage.db.infusions.filter(i => !i.isCustom && i.enabled).count(count => {
-                $("#current-infused-items").text(count);
-            });
+            Storage.db.infusions
+                .filter((i) => !i.isCustom && i.enabled)
+                .count((count) => {
+                    $("#current-infused-items").text(count);
+                });
         };
         Infusion.updateAttunedItems = function () {
-            Storage.db.infusions.filter(i => i.attunement && i.enabled).count(count => {
-                $("#current-attuned-items").text(count);
-            });
+            Storage.db.infusions
+                .filter((i) => i.attunement && i.enabled)
+                .count((count) => {
+                    $("#current-attuned-items").text(count);
+                });
         };
     }
 
     generateHtml(isLast) {
         let row = $("<div>", {
-            class: "row bg-dark-subtle g-0 infusion"
+            class: "row bg-dark-subtle g-0 infusion",
         });
         let left_col = $("<div>", {
-            class: "col border border-3 border-black align-middle" + (isLast ? "" : " border-bottom-0")
+            class: "col border border-3 border-black align-middle" + (isLast ? "" : " border-bottom-0"),
         });
         let name_div = $("<div>", {
-            class: "position-relative top-50 start-50 translate-middle"
+            class: "position-relative top-50 start-50 translate-middle",
         }).html(this.name);
         if (this.attunement) {
-            name_div.prepend("<span class=\"badge text-bg-secondary\">A</span>");
+            name_div.prepend('<span class="badge text-bg-secondary">A</span>');
         }
 
         left_col.append(name_div);
         row.append(left_col);
 
         let right_col = $("<div>", {
-            class: "col-3 border border-start-0 border-3 border-black" + (isLast ? "" : " border-bottom-0")
+            class: "col-3 border border-start-0 border-3 border-black" + (isLast ? "" : " border-bottom-0"),
         });
         let btn_group = $("<div>", {
             class: "btn-group w-100",
             role: "group",
-            "aria-label": "Basic checkbox toggle button group"
+            "aria-label": "Basic checkbox toggle button group",
         });
         let btn = $("<input>", {
             type: "checkbox",
             class: "btn-check",
             id: "infusion-" + this.index,
-            autocomplete: "off"
+            autocomplete: "off",
         });
 
         let id = this.id;
@@ -62,12 +66,14 @@ class Infusion {
         if (this.isFix) {
             btn.prop("checked", true);
             btn.prop("disabled", true);
+        } else {
+            btn.prop("checked", this.enabled);
         }
         btn_group.append(btn);
 
         let label = $("<label>", {
             class: "btn btn-outline-secondary rounded-0",
-            for: "infusion-" + this.index
+            for: "infusion-" + this.index,
         }).html("Used");
         btn_group.append(label);
         right_col.append(btn_group);
@@ -75,7 +81,9 @@ class Infusion {
 
         let divider = $("<div>", { class: "w-100" });
         row.append(divider);
-        let text_col = $("<p>", { class: "col border border-3 border-black text-start mb-0 " + (isLast ? "border-top-0" : "border-bottom-0") }).html(this.text);
+        let text_col = $("<p>", { class: "col border border-3 border-black text-start mb-0 " + (isLast ? "border-top-0" : "border-bottom-0") }).html(
+            this.text,
+        );
         text_col.hide();
         row.append(text_col);
 
